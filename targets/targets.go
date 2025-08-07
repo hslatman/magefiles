f910bc9156ec218d167cf07c5f432b1b6ed563b1
Herman Slatman <hermanslatman@hotmail.com>
2025-08-07 10:43:07 +0200

    Add `tools` target

--- targets/targets.go
+++ targets/targets.go
@@ -1,6 +1,28 @@
 package targets
 
-import "github.com/magefile/mage/sh"
+import (
+	"fmt"
+	"os"
+
+	"github.com/magefile/mage/sh"
+)
+
+const (
+	toolsMod = "./.tools/go.mod"
+	toolsGo  = "tools.go"
+)
+
+// Tools ensures the tools get installed
+func Tools() error {
+	_, err := os.Stat(toolsMod)
+	if err == nil {
+		return sh.RunV("go", "mod", "tidy", fmt.Sprintf("-modfile=%s", toolsMod))
+	}
+
+	// TODO: add support for tools.go?
+
+	return sh.RunV("go", "mod", "tidy")
+}
 
 // Tidy ensures the dependencies are available
 func Tidy() error {
